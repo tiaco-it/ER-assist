cce982ae004fb0eae1e28d9cdfeb979e26a30a4e
thormartin91 <thorma91@gmail.com>
2015-10-26 21:28:31 +0100

    display urls in links-view

--- app/client/app.js
+++ app/client/app.js
@@ -254,6 +254,19 @@ Template.law.helpers({
 	}
 })
 
+Template.links.helpers({
+	'links': function() {
+		var urls = Urls.find({})
+		return urls && urls
+	}
+});
+
+Template.link.helpers({
+	'thisLink': function() {
+		return Urls.findOne(Router.current().params._id)
+	}
+})
+
 Template.ttabs.onRendered( function () {
     Session.set('currentTab', 'end')
 })
