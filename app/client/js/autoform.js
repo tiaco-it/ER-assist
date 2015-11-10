b0ee65d08d63bab0f92f7dad185f5c8e60d15205
areeh <areeh@stud.ntnu.no>
2015-11-10 15:31:54 +0100

    error handling and starting previous added items display

--- app/client/js/autoform.js
+++ app/client/js/autoform.js
@@ -17,7 +17,7 @@ insertLink = function(from, mark, to) {
     console.log(obj)
     Meteor.call('addLink2', obj, function(error, result) {
         if (error) {
-            alert(error.reason)
+            alert(error.reason);
             console.log('GOT ERROR AT INSERT');
         } else {
             console.log('GOT SUCCESS AT INSERT')
@@ -43,15 +43,17 @@ AutoForm.hooks({
     insertStartcaseForm: {
         onSubmit: function(insertDoc) {
             insert = insertDoc;
-            Session.set('From', insertDoc.text);
-            console.log('From got set');
-            console.log(Session.get('From'));
-            Meteor.call('addStartcase', insertDoc, function(error, result) {
-                if (error) {
-                console.log(error.reason)
-                alert(error.reason);
-                }
-            });
+            if (typeof insert !== 'undefined') {
+                Session.set('From', insertDoc.text);
+                console.log('From got set');
+                console.log(Session.get('From'));
+                Meteor.call('addStartcase', insertDoc, function(error, result) {
+                    if (error) {
+                    console.log(error.reason)
+                    alert(error.reason);
+                    }
+                }); 
+            }
             this.done();
             return false;
         },
@@ -61,11 +63,15 @@ AutoForm.hooks({
             console.log('STARTING SUBMIT')
         },
         endSubmit: function() {
-            //First item inserted into db, so clean it if cancelled
-            Session.set('cleanDB', true);
-            addedItems["scases"].push(insert);
-            Router.current().render('pathButtons', {to: 'choices'});
-            console.log('DONE')
+            if (typeof insert !== 'undefined') {
+                //First item inserted into db, so clean it if cancelled
+                Session.set('cleanDB', true);
+                addedItems["scases"].push(insert);
+                Router.current().render('pathButtons', {to: 'choices'});
+                Router.current().render('blank', {to: 'add'});
+                console.log('DONE');
+                insert = undefined;
+            }
         }
     }
 });
@@ -73,36 +79,37 @@ AutoForm.hooks({
 AutoForm.hooks({
     insertFirstFilterForm: {
         onSubmit: function(insertDoc) {
-            console.log('onSubmit');
-            insertDoc["number_of_outcomes"] = 2;
             insert = insertDoc;
-            console.log('Altered doc');
-            console.log(insertDoc);
-            Session.set('To', insertDoc.text);
-            Meteor.call('addFilter', insertDoc, function(error, result) {
-                if (error) {
-                    console.log('failed first')
-                    alert(error.reason);
-                }   
-            });
+            if (typeof insert !== 'undefined') {
+                console.log('onSubmit');
+                insertDoc["number_of_outcomes"] = 2;
+                console.log('Altered doc');
+                console.log(insertDoc);
+                Session.set('To', insertDoc.text);
+                Meteor.call('addFilter', insertDoc, function(error, result) {
+                    if (error) {
+                        console.log('failed first')
+                        alert(error.reason);
+                    }   
+                });
+            }
             this.done();
             return false;
-        },
-        onSuccess: function(formType, result) {
-            console.log(Session.get('To'));
         },
         beginSubmit: function() {
             console.log('STARTING SUBMIT 1')
         },
         endSubmit: function() {
-            addedItems["filters"].push(insert);
-            console.log('DONE')
-            var from = Startcases.findOne({ 'text': Session.get('From')});
-            var to = Filters.findOne({ 'text': Session.get('To')});
-            var mark = "";
-            var isInserted = insertLink(from, mark, to);
-            console.log(isInserted);
-            if (isInserted) {
+            if (typeof insert !== 'undefined') {
+                addedItems["filters"].push(insert);
+                console.log('DONE')
+                var from = Startcases.findOne({ 'text': Session.get('From')});
+                var to = Filters.findOne({ 'text': Session.get('To')});
+                var mark = "";
+                var isInserted = insertLink(from, mark, to);
+                console.log(isInserted);
+                insert = undefined;
+                if (isInserted) {
                     console.log('FirstLinkAddSuccess');
                     Router.current().render('itemToAdd', {to: 'next'});
                     Router.current().render('addQ', {to: 'forms'});
@@ -110,8 +117,9 @@ AutoForm.hooks({
                     pathQueue.push('JA');
                     pathQueue.push('NEI');
                     topElement.push('JA');
-            } else {
-                alert('link connection failed');
+                } else {
+                    alert('link connection failed');
+                }
             }
         }
     }
@@ -120,56 +128,58 @@ AutoForm.hooks({
 AutoForm.hooks({
     insertFilterForm: {
         onSubmit: function(insertDoc) {
-            insertDoc["number_of_outcomes"] = 2;
             insert = insertDoc;
-            console.log(pathQueue[0]);
-            console.log(topElement[0]);
-            if (pathQueue[0] === topElement[0]) {
-                if (nextFrom.length > 0) {
-                    Session.set('From', nextFrom.shift());
+            if (typeof insert !== 'undefined') {
+                insertDoc["number_of_outcomes"] = 2;
+                console.log(pathQueue[0]);
+                console.log(topElement[0]);
+                if (pathQueue[0] === topElement[0]) {
+                    if (nextFrom.length > 0) {
+                        Session.set('From', nextFrom.shift());
+                    } else {
+                        Session.set('From', Session.get('To'));
+                    }
+                    Session.set('To', insertDoc.text);
+                    topElement.shift();
                 } else {
-                    Session.set('From', Session.get('To'));
+                    console.log('GOT CORRECT AT FILTER')
+                    Session.set('To', insertDoc.text);
                 }
-                Session.set('To', insertDoc.text);
-                topElement.shift();
-            } else {
-                console.log('GOT CORRECT AT FILTER')
-                Session.set('To', insertDoc.text);
+                console.log('got to method call')
+                Meteor.call('addFilter', insertDoc, function(error, result) {
+                    if (error) {
+                        console.log('Failed second')
+                        console.log(error.reason);
+                    }
+                });
+                console.log('finished method call')
             }
-            console.log('got to method call')
-            Meteor.call('addFilter', insertDoc, function(error, result) {
-                if (error) {
-                    console.log('Failed second')
-                    console.log(error.reason);
-                }
-            });
-            console.log('finished method call')
             this.done();
             return false;
-        },
-        onSuccess: function(formType, result) {
-            console.log(Session.get('To'));
         },
         beginSubmit: function() {
             console.log('STARTING SUBMIT 2')
         },
         endSubmit: function() {
-            addedItems["filters"].push(insert);
-            nextFrom.push(insert);
-            console.log('DONE')
-            var from = Filters.findOne({ 'text': Session.get('From')});
-            var to = Filters.findOne({ 'text': Session.get('To')});
-            var mark = pathQueue[0];
-            var isInserted = insertLink(from, mark, to);
-            console.log(isInserted);
-            if (isInserted) {
-                console.log('NextLinkAddSuccess');
-                pathQueue.shift();
-                pathQueue.push('JA');
-                pathQueue.push('NEI');
-                topElement.push('JA');
-            } else {
-                alert('link connection failed');
+            if (typeof insert !== 'undefined') {
+                addedItems["filters"].push(insert);
+                nextFrom.push(insert);
+                console.log('DONE')
+                var from = Filters.findOne({ 'text': Session.get('From')});
+                var to = Filters.findOne({ 'text': Session.get('To')});
+                var mark = pathQueue[0];
+                var isInserted = insertLink(from, mark, to);
+                console.log(isInserted);
+                insert = undefined;
+                if (isInserted) {
+                    console.log('NextLinkAddSuccess');
+                    pathQueue.shift();
+                    pathQueue.push('JA');
+                    pathQueue.push('NEI');
+                    topElement.push('JA');
+                } else {
+                    alert('link connection failed');
+                }
             }
         }
     }
@@ -178,66 +188,69 @@ AutoForm.hooks({
 AutoForm.hooks({
     insertFilterNForm: {
         onSubmit: function(insertDoc) {
-            console.log('got to Submit')
             insert = insertDoc;
-            //If the next item to link is the first to be connected to a new "from" node (TopElement)
-            //Then shuffle the from node down the tree one level
-            if (pathQueue[0] === topElement[0] && addedItems['filters'].length > 0) {
-                if (nextFrom.length > 0) {
-                    Session.set('From', nextFrom.shift());
-                    Session.set('To', insertDoc.text);
+            if (typeof insert !== 'undefined') {
+                console.log('got to Submit')
+                //If the next item to link is the first to be connected to a new "from" node (TopElement)
+                //Then shuffle the from node down the tree one level
+                if (pathQueue[0] === topElement[0] && addedItems['filters'].length > 0) {
+                    if (nextFrom.length > 0) {
+                        Session.set('From', nextFrom.shift());
+                        Session.set('To', insertDoc.text);
+                    } else {
+                        Session.set('From', Session.get('To'));
+                        Session.set('To', insertDoc.text);
+                    }
+                    topElement.shift();
+                //Otherwise, just shift To along same layer
                 } else {
-                    Session.set('From', Session.get('To'));
+                    console.log('not equal to top');
                     Session.set('To', insertDoc.text);
                 }
-                topElement.shift();
-            //Otherwise, just shift To along same layer
-            } else {
-                console.log('not equal to top');
-                Session.set('To', insertDoc.text);
+                Meteor.call('addFilter', insertDoc, function(error, result) {
+                    if (error) {
+                        console.log(error.reason);
+                    }
+                });
             }
-            Meteor.call('addFilter', insertDoc, function(error, result) {
-                if (error) {
-                    console.log(error.reason);
-                }
-            });
             this.done();
             return false;
-        },
-        onSuccess: function(formType, result) {
         },
         beginSubmit: function() {
             console.log('STARTING SUBMIT 2')
         },
         endSubmit: function() {
-            markCount = insert.number_of_outcomes;
-            addedItems["filters"].push(insert);
-            nextFrom.push(insert);
-            console.log('DONE')
-            var from = Startcases.findOne({ 'text': Session.get('From')});
-            if (from === undefined) {
-                var from = Filters.findOne({ 'text': Session.get('From')});
-            }
-            console.log(from);
-            var to = Filters.findOne({ 'text': Session.get('To')});
-            if (pathQueue.length > 0) {
-                var mark = pathQueue[0];
-            } else {
-                var mark = "";
-            }
-            if (insertLink(from, mark, to)) {
-                    Session.set('firstFilterCreated', true);
-                    console.log('NLinkAddSuccess');
-                    pathQueue.shift();
-                    Router.current().render('answerOptions', {to: 'next'});
-                    Router.current().render('blank', {to: 'choices'});
-                    Router.current().render('addMarks', {to: 'forms' });
-            } else {
-                alert('Couldnt link n options filter');
-            }
-            //if this was the first connection, shift From down one layer in the tree
-            if (addedItems['filters'].length == 1) {
-                Session.set('From', Session.get('To'));
+            if (typeof insert !== 'undefined') {
+                markCount = insert.number_of_outcomes;
+                addedItems["filters"].push(insert);
+                nextFrom.push(insert);
+                console.log('DONE')
+                var from = Startcases.findOne({ 'text': Session.get('From')});
+                if (from === undefined) {
+                    var from = Filters.findOne({ 'text': Session.get('From')});
+                }
+                console.log(from);
+                var to = Filters.findOne({ 'text': Session.get('To')});
+                if (pathQueue.length > 0) {
+                    var mark = pathQueue[0];
+                } else {
+                    var mark = "";
+                }
+                insert = undefined;
+                if (insertLink(from, mark, to)) {
+                        Session.set('firstFilterCreated', true);
+                        console.log('NLinkAddSuccess');
+                        pathQueue.shift();
+                        Router.current().render('answerOptions', {to: 'next'});
+                        Router.current().render('blank', {to: 'choices'});
+                        Router.current().render('addMarks', {to: 'forms' });
+                } else {
+                    alert('Couldnt link n options filter');
+                }
+                //if this was the first connection, shift From down one layer in the tree
+                if (addedItems['filters'].length == 1) {
+                    Session.set('From', Session.get('To'));
+                }
             }
         }
     }
@@ -247,15 +260,18 @@ AutoForm.hooks({
     insertLawForm: {
         onSubmit: function(insertDoc) {
             insert = insertDoc;
-            Session.set('From', Session.get('To'));
-            Session.set('To', insertDoc.paragraph);
-            console.log('FromTo')
-            console.log(Session.get('From'))
-            console.log(Session.get('To'))
-            Meteor.call('addLaw', insertDoc, function(error, result) {
-                if (error) alert(error.reason);
-            });
-            $(".back-button").click();
+            if (typeof insert !== 'undefined') {
+                Session.set('From', Session.get('To'));
+                Session.set('To', insertDoc.paragraph);
+                console.log('FromTo')
+                console.log(Session.get('From'))
+                console.log(Session.get('To'))
+                Meteor.call('addLaw', insertDoc, function(error, result) {
+                    if (error) alert(error.reason);
+                });
+                $(".back-button").click();
+            }
+            this.done();
             return false;
         },
         onSuccess: function(formType, result) {
@@ -265,8 +281,11 @@ AutoForm.hooks({
             console.log('STARTING SUBMIT')
         },
         endSubmit: function() {
-            addedItems["laws"].push(insert)
-            console.log('DONE')
+            if (typeof insert !== 'undefined') {
+                addedItems["laws"].push(insert)
+                console.log('DONE')
+                insert = undefined;
+            }
         }
     }
 });
@@ -274,7 +293,10 @@ AutoForm.hooks({
 AutoForm.hooks({
         insertMarkForm: {
         onSubmit: function(insertDoc) {
-            marks.push(insertDoc.mark);
+            insert = insertDoc;
+            if (typeof insert !== 'undefined') {
+                marks.push(insertDoc.mark);
+            }
             this.done();
             return false;
         },
@@ -283,17 +305,20 @@ AutoForm.hooks({
         beginSubmit: function() {
         },
         endSubmit: function() {
-            markCount-=1;
-            if (markCount < 1) {
-                topElement.push(marks[0]);
-                while (marks.length > 0) {
-                    pathQueue.push(marks.shift());
+            if (typeof insert !== 'undefined') {
+                markCount-=1;
+                if (markCount < 1) {
+                    topElement.push(marks[0]);
+                    while (marks.length > 0) {
+                        pathQueue.push(marks.shift());
+                    }
+                    Router.current().render('itemToAdd', {to: 'next'});
+                    Router.current().render('pathButtons', {to: 'choices'});
+                    Router.current().render('blank', {to: 'forms'});
+                } else {
+                    Router.current().render('addMarks', {to: 'forms'});
                 }
-                Router.current().render('itemToAdd', {to: 'next'});
-                Router.current().render('pathButtons', {to: 'choices'});
-                Router.current().render('blank', {to: 'forms'});
-            } else {
-                Router.current().render('addMarks', {to: 'forms'});
+                insert = undefined;
             }
         }
     }
@@ -306,6 +331,7 @@ AutoForm.hooks({
                 if (error) alert(error.reason);
             });
             $(".back-button").click();
+            this.done();
             return false;
         }
     }
@@ -318,6 +344,7 @@ AutoForm.hooks({
                 if (error) alert(error.reason);
             });
             $(".back-button").click();
+            this.done();
             return false;
         }
     }
@@ -331,6 +358,7 @@ AutoForm.hooks({
                 if (error) alert(error.reason);
             });
             $(".back-button").click();
+            this.done();
             return false;
         }
     }
@@ -344,6 +372,7 @@ AutoForm.hooks({
                 if (error) alert(error.reason);
             });
             $(".back-button").click();
+            this.done();
             return false;
         }
     }
@@ -357,6 +386,7 @@ AutoForm.hooks({
                 if (error) alert(error.reason);
             });
             $(".back-button").click();
+            this.done();
             return false;
         }
     }
@@ -370,6 +400,7 @@ AutoForm.hooks({
                 if (error) alert(error.reason);
             });
             $(".back-button").click();
+            this.done();
             return false;
         }
     }
@@ -383,6 +414,7 @@ AutoForm.hooks({
                 if (error) alert(error.reason);
             });
             $(".back-button").click();
+            this.done();
             return false;
         }
     }
